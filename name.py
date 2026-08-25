25789ac3a16d4ed46b3be39ea0b6eadf3347c015
shreyas_naik <vhgshreyas@gmail.com>
2026-08-25 11:50:23 +0530

    modified

--- name.py
+++ name.py
@@ -1,4 +1,8 @@
 name= input("enter your name")
 surname= input("enter your surname")
 contact = input("enter your contact number")
+
 print ("your full name is:",name+" "+surname)
+print ("your contact number is:", contact)
+print("---")
+
