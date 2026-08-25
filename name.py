9d7024a45337dcd358d66912721f785724e45782
shreyas242007 <vhgshreyas@gmail.com>
2026-08-25 11:52:50 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,8 +1,9 @@
 name= input("enter your name")
 surname= input("enter your surname")
 contact = input("enter your contact number")
-
+city = input("enter your city")
 print ("your full name is:",name+" "+surname)
 print ("your contact number is:", contact)
 print("---")
+print ("your city is:", city)
 
